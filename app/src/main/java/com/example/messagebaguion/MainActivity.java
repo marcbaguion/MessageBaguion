bec3edb57a4b3035b4cb1d8a15aedb03986c0413
marcbaguion <108505608+marcbaguion@users.noreply.github.com>
2022-07-20 16:32:29 +0800

    Milestone 3: Use implicit intents

--- app/src/main/java/com/example/messagebaguion/MainActivity.java
+++ app/src/main/java/com/example/messagebaguion/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.messagebaguion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +18,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSendListenerMethod();
+        btnCallListenerMethod();
+        btnMapListenerMethod();
+        btnWebListenerMethod();
+    }
+
+    private void btnWebListenerMethod() {
+        ImageButton btnWeb = findViewById(R.id.btnWeb);
+        Uri webpage = Uri.parse("https://www.google.co.kr");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void btnMapListenerMethod() {
+        ImageButton btnMap = findViewById(R.id.btnMap);
+        Uri map = Uri.parse("geo:0,0?q=City+of+Ormoc+City+Ormoc+Philippines");
+        Intent intent = new Intent(Intent.ACTION_VIEW, map);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+    private void btnCallListenerMethod() {
+        ImageButton btnCall = findViewById(R.id.btnCall);
+        Uri number = Uri.parse("tel:09192066499");
+        Intent intent = new Intent(Intent.ACTION_DIAL, number);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 
     private void btnSendListenerMethod() {
+
         Intent intent = new Intent(this, MessageActivity.class);
         Button btnSend = findViewById(R.id.btnSend);
         btnSend.setOnClickListener(new View.OnClickListener() {
